e689c36609da300ae0715f7863e0bd9b218aedbd
zaheralmattrud <zaheralmattrud@gmail.com>
2024-02-10 20:06:23 +0200

    add validation to Employee Dto

--- src/main/java/com/example/EmployeeRestApisWithSpringBoot/dto/EmployeeDto.java
+++ src/main/java/com/example/EmployeeRestApisWithSpringBoot/dto/EmployeeDto.java
@@ -1,5 +1,6 @@
 package com.example.EmployeeRestApisWithSpringBoot.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,21 @@ import lombok.NoArgsConstructor;
 @Data
 public class EmployeeDto {
 
+
+    @NotNull
+    @NotBlank(message = "Please provide a firstName")
+    @Max(value = 20 , message = " firstName must 20 character maximum")
+    @Min(value = 20 , message = " firstName must  grater than 3 character ")
     private String firstName ;
 
+    @NotNull
+    @NotBlank(message = "Please provide a lastName")
+    @Max(value = 20 , message = " lastName must 20 character maximum")
+    @Min(value = 20 , message = " lastName must  grater than 3 character ")
     private String lastName  ;
 
+    @NotNull
+    @NotBlank(message = "Please provide a salary")
+    @Size(min = 1,message = "salary must equal or grater than 1 SYP ")
     private double salary ;
 }
