81c0e5681e7390e911f826e97470d5fa8cf4329b
zaheralmattrud <zaheralmattrud@gmail.com>
2024-02-10 20:27:09 +0200

    handle validation error

--- src/main/java/com/example/EmployeeRestApisWithSpringBoot/controller/EmployeeController.java
+++ src/main/java/com/example/EmployeeRestApisWithSpringBoot/controller/EmployeeController.java
@@ -6,9 +6,12 @@ import com.example.EmployeeRestApisWithSpringBoot.exception.EmployeeNotFoundExce
 import com.example.EmployeeRestApisWithSpringBoot.model.Employee;
 import com.example.EmployeeRestApisWithSpringBoot.repository.EmployeeRepository;
 import com.example.EmployeeRestApisWithSpringBoot.service.EmployeeService;
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,16 +28,35 @@ public class EmployeeController
 
 
 @PostMapping("/create")
-    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+    public ResponseEntity<String> createEmployee(@RequestBody @Valid EmployeeDto employeeDto , BindingResult bindingResult){
 
-        return employeeService.createEmployee(employeeDto);
+    if (bindingResult.hasErrors()) {
+        for (ObjectError error : bindingResult.getAllErrors()) {
+            String errorMessage = error.getDefaultMessage();
+            return ResponseEntity.badRequest().body( errorMessage );
+        }
+        //return ResponseEntity.badRequest().body("Validation failed");
+    }
+
+    employeeService.createEmployee(employeeDto);
+
+    return ResponseEntity.ok("Employee Created Successfully");
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable Long id ,@RequestBody EmployeeDto employeeDto){
+    public ResponseEntity<String> updateEmployee(@PathVariable Long id ,@RequestBody @Valid EmployeeDto employeeDto , BindingResult bindingResult){
 
+        if (bindingResult.hasErrors()) {
+            for (ObjectError error : bindingResult.getAllErrors()) {
+                String errorMessage = error.getDefaultMessage();
+                return ResponseEntity.badRequest().body( errorMessage );
+            }
+          //  return ResponseEntity.badRequest().body("Validation failed");
+        }
 
-        return employeeService.updateEmployee(id , employeeDto);
+        employeeService.updateEmployee(id , employeeDto);
+
+        return ResponseEntity.ok("Employee Updated Successfully");
 
     }
 
